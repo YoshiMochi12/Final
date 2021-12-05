2da59f19df74864f2beeb2583f39794f8e82b1d7
MartinB1244 <90154891+MartinB1244@users.noreply.github.com>
2021-12-05 17:41:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -87,12 +87,8 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-    
-@app.route("/")
-def table():
-    return render_template("dashboard.html")
 
-@app.route("/dashboard", methods=["GET", "POST"])
+@app.route("/table", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
         form = request.form
